769d187cae98fc6c7ef919884f00a94b36723549
Vladimir V. Bychkov <github@bychkov.name>
2024-04-16 04:07:42 +0200

    extracted reused methods

--- src/main/java/name/bychkov/gmail/ConsoleSender.java
+++ src/main/java/name/bychkov/gmail/ConsoleSender.java
@@ -1,9 +1,9 @@
 package name.bychkov.gmail;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Properties;
 
@@ -15,6 +15,7 @@ import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
 import com.google.api.services.gmail.model.Message;
 
+import jakarta.mail.Message.RecipientType;
 import jakarta.mail.MessagingException;
 import jakarta.mail.Session;
 import jakarta.mail.internet.AddressException;
@@ -24,35 +25,54 @@ import jakarta.mail.internet.MimeMessage;
 public class ConsoleSender {
 
 	public static void main(String[] args) throws IOException, AddressException, MessagingException {
+		// read console parameters
 		String applicationName = System.getProperty("application.name");
 		String delegate = System.getProperty("delegate");
-		String serviceAccountKey = System.getProperty("service.account.key");
+		File serviceAccountKeyFile = new File(System.getProperty("service.account.key"));
 		String emailSubject = System.getProperty("email.subject");
 		String emailBody = System.getProperty("email.body");
 		String emailRecipient = System.getProperty("email.recipient");
 
+		// construct JavaMail message
+		MimeMessage javaMailMessage = getJavaMailMessage(emailRecipient, emailSubject, emailBody);
+
+		// connect to GMail
+		Gmail client = getGMailClient(applicationName, serviceAccountKeyFile, delegate);
+		
+		// send message
+		Message gmailMessage = getGMailMessage(javaMailMessage);
+		client.users().messages().send(delegate, gmailMessage).execute();
+	}
+
+	private static Message getGMailMessage(MimeMessage javaMailMessage) throws IOException, MessagingException {
+		Message message = new Message();
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		javaMailMessage.writeTo(buffer);
+		message.encodeRaw(buffer.toByteArray());
+		return message;
+	}
+
+	private static Gmail getGMailClient(String applicationName, File serviceAccountKeyFile, String delegate)
+			throws IOException {
 		HttpTransport transport = Utils.getDefaultTransport();
 		JsonFactory jsonFactory = Utils.getDefaultJsonFactory();
 		GoogleCredential credentials = GoogleCredential
-				.fromStream(Files.newInputStream(Paths.get(serviceAccountKey))
-						, transport, jsonFactory)
+				.fromStream(new FileInputStream(serviceAccountKeyFile), transport, jsonFactory)
 				.createScoped(Arrays.asList(GmailScopes.GMAIL_SEND)).createDelegated(delegate);
 		credentials.refreshToken();
-		Gmail client = new Gmail.Builder(transport, jsonFactory, credentials).setApplicationName(applicationName)
-				.build();
+		Gmail client = new Gmail.Builder(transport, jsonFactory, credentials)
+				.setApplicationName(applicationName).build();
+		return client;
+	}
 
+	private static MimeMessage getJavaMailMessage(String recipient, String subject, String body)
+			throws AddressException, MessagingException {
 		Properties props = new Properties();
 		Session session = Session.getDefaultInstance(props, null);
 		MimeMessage email = new MimeMessage(session);
-		email.addRecipient(jakarta.mail.Message.RecipientType.TO, new InternetAddress(emailRecipient));
-		email.setSubject(emailSubject);
-		email.setText(emailBody);
-
-		Message message = new Message();
-		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-		email.writeTo(buffer);
-		message.encodeRaw(buffer.toByteArray());
-
-		client.users().messages().send(delegate, message).execute();
+		email.addRecipient(RecipientType.TO, new InternetAddress(recipient));
+		email.setSubject(subject);
+		email.setText(body);
+		return email;
 	}
 }
